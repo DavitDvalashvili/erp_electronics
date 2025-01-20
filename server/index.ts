c898683709dd3b42fc2ac1ebd071c6e6ea981fd0
davit dvalashvili <davitdvalashvili1996@gmail.com>
2025-01-21 02:45:02 +0400

    fix: fix merge conflict

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import componentRouter from "./routes/component.route";
-<<<<<<< HEAD
 import deviceRouter from "./routes/device.route";
-=======
->>>>>>> f8a9e46c928d6fa62a119681ce8c02c72b8ac3ba
 
 const { PORT } = process.env;
 
@@ -11,10 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(componentRouter);
-<<<<<<< HEAD
 app.use(deviceRouter);
-=======
->>>>>>> f8a9e46c928d6fa62a119681ce8c02c72b8ac3ba
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
