18eb00d5881b43807fe0ef0037317e1e39bf61a0
davit dvalashvili <davitdvalashvili1996@gmail.com>
2025-01-21 03:59:30 +0400

    feat: add addDevice and updateDevice functionality and routes

--- server/controllers/device.controller.ts
+++ server/controllers/device.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createConnection } from "../db/database";
-//import { DeviceInfo } from "../@types/global";
+import { DeviceInfo } from "../@types/global";
 import { ResponseStatus } from "../@types/type";
 
 export const getDevices = async (req: Request, res: Response) => {
@@ -109,6 +109,166 @@ export const getDevice = async (req: Request, res: Response) => {
     res
       .status(500)
       .send({ error: "An error occurred while fetching components." });
+  } finally {
+    if (conn) conn.release();
+  }
+};
+
+export const addDevice = async (req: Request, res: Response): Promise<void> => {
+  let conn;
+  let status: ResponseStatus;
+
+  const {
+    name,
+    purpose,
+    electrical_supply,
+    size,
+    available_quantity,
+    unit_cost,
+  } = <DeviceInfo>req.body;
+
+  const deviceInfo = [
+    name,
+    purpose,
+    electrical_supply,
+    size,
+    available_quantity,
+    unit_cost,
+  ];
+
+  try {
+    conn = await createConnection();
+
+    if (!name || !available_quantity) {
+      res.status(404).send({ message: "Empty required fields" });
+    }
+
+    let existingDevices = await conn.query(
+      `SELECT * FROM devices d WHERE d.name = ? `,
+      name
+    );
+
+    if (existingDevices.length > 0) {
+      status = {
+        status: "insert_exists",
+        message: "Device already exists",
+      };
+      res.status(409).json(status);
+      return;
+    }
+
+    const insertQuery = `
+    INSERT INTO devices 
+     (name, purpose, electrical_supply, size, available_quantity, unit_cost) 
+      VALUES 
+     (?, ?, ?, ?, ?, ?)
+    `;
+
+    await conn
+      .query(insertQuery, [...deviceInfo])
+      .then(() => {
+        status = {
+          status: "inserted",
+          message: "Device inserted successfully",
+        };
+        res.send(status);
+      })
+      .catch((err) => {
+        status = {
+          status: "insert_error",
+          message: "An error occurred while adding device.",
+        };
+        res.send(status);
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+    status = {
+      status: "insert_error",
+      message: "An error occurred while adding device.",
+    };
+    res.status(500).send(status);
+  } finally {
+    if (conn) conn.release();
+  }
+};
+
+export const updateDevice = async (req: Request, res: Response) => {
+  let conn;
+  let id = req.params.id as string;
+
+  let status: ResponseStatus;
+
+  const {
+    name,
+    purpose,
+    electrical_supply,
+    size,
+    available_quantity,
+    unit_cost,
+  } = <DeviceInfo>req.body;
+
+  const deviceInfo = [
+    name,
+    purpose,
+    electrical_supply,
+    size,
+    available_quantity,
+    unit_cost,
+  ];
+
+  try {
+    conn = await createConnection();
+
+    let existingDevice = await conn.query(
+      `SELECT * FROM devices WHERE name = ? AND id != ?`,
+      [name, id]
+    );
+
+    if (existingDevice.length > 0) {
+      status = {
+        status: "update_exists",
+        message: "Device already exists",
+      };
+      res.status(409).json(status);
+      return;
+    }
+
+    const updateQuery = `
+      UPDATE devices
+      SET 
+      name = ?,
+      purpose = ?,
+      electrical_supply = ?,
+      size = ?,
+      available_quantity = ?,
+      unit_cost = ?
+      WHERE id = ?`;
+
+    await conn
+      .query(updateQuery, [...deviceInfo, id])
+      .then(() => {
+        status = {
+          status: "updated",
+          message: "Device updated successfully",
+        };
+        res.send(status);
+      })
+      .catch((err) => {
+        status = {
+          status: "update_error",
+          message: "An error occurred while updating device.",
+        };
+        res.send(status);
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+    status = {
+      status: "update_error",
+      message: "An error occurred while updating device.",
+    };
+    res.status(500).send(status);
   } finally {
     if (conn) conn.release();
   }
--- server/routes/device.route.ts
+++ server/routes/device.route.ts
@@ -1,9 +1,18 @@
-import { getDevices, getDevice } from "../controllers/device.controller";
+import {
+  getDevices,
+  getDevice,
+  addDevice,
+  updateDevice,
+  deleteDevice,
+} from "../controllers/device.controller";
 import { Router } from "express";
 
 const deviceRouter = Router();
 
 deviceRouter.get("/getDevices", getDevices);
 deviceRouter.get("/getDevice/:id", getDevice);
+deviceRouter.post("/addDevice", addDevice);
+deviceRouter.post("/updateDevice/:id", updateDevice);
+deviceRouter.delete("/deleteDevice/:id", deleteDevice);
 
 export default deviceRouter;
